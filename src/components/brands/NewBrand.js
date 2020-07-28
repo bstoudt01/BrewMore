37f798659374b69aca69c1e7faea4f30b5604455
Brett Stoudt <Bstoudt01@gmail.com>
2020-07-28 14:11:46 -0500

    handlefield change is now watching all inputs and gathering data back to 1 setState, i will likely have grain and weight go into a seperate set state that includes brandId

--- src/components/brands/NewBrand.js
+++ src/components/brands/NewBrand.js
@@ -14,7 +14,7 @@ import GrainManager from '../../modules/GrainManager';
 import BrandManager from '../../modules/BrandManager';
   
 const NewBrand = () => {
-
+    const [brand, setBrand] = useState([])
     const [grainSelects, setGrainSelects] = useState([])
     const [grainFieldsArray, setGrainFieldsArray] = useState([])
     const [usedGrains, setUsedGrains] = useState([{grainId:"", weight:""}])
@@ -22,7 +22,7 @@ const NewBrand = () => {
 
     const handleAdd = () => {
         const values = [...grainFieldsArray ];
-        values.push({ value: grainFieldsArray.id });
+        values.push({ grainId: grainFieldsArray.id, weight: grainFieldsArray.weight });
         setGrainFieldsArray(values);
       }
 
@@ -40,12 +40,36 @@ const NewBrand = () => {
     //     // values.splice(i, 1);
     //     // setGrainFieldsArray(values);
     //   }
+    
 
-      const handleChange=(i, event)  => {
-        const values = [...grainFieldsArray];
-        values[i].value = event.target.value;
-        setGrainFieldsArray(values);
+        //Handle Change of Inputs on Form
+        //anytime you have an event all of the stuff is passed along 
+        //state to change set equal to value and pass it in
+        // brred and name are inside our state, so any change to those values causes setAnimal to run with stateToChange passed through
+        // it watches you type into the input and holds onto that as stateToChange and then when you hit enter it subbmits those and creates a new database item.
+      const handleGrainChange=(i, event)  => {
+        console.log("what is evt", event)
+        console.log("what is i?", i)
+        const stateToChange = [...grainFieldsArray];
+        console.log("stateToChange", stateToChange);
+        stateToChange[i].grainId = event.target.value;
+        stateToChange[i].weight = event.target.value;
+        setGrainFieldsArray(stateToChange);
       }
+
+      const handleFieldChange = evt => {
+    
+        console.log("what is evt", evt)
+        //anytime you have an event all of the stuff is passed along 
+        //state to change set equal to value and pass it in
+        // brred and name are inside our state, so any change to those values causes setAnimal to run with stateToChange passed through
+        // it watches you type into the input and holds onto that as stateToChange and then when you hit enter it subbmits those and creates a new database item.
+      const stateToChange = { ...brand };
+      console.log("stateToChange", stateToChange);
+      console.log("stateToChange", stateToChange);
+      stateToChange[evt.target.id] = evt.target.value;
+      setBrand(stateToChange);
+    };
     // const addNewGrainInput = () => {
     //     //setGrainSelects(grainSelects=> grainSelects += className="mb+map");
     //   };
@@ -78,7 +102,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Name:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="New Brand Name" />
+                        <Form.Control type="text" id="name" placeholder="New Brand Name" onChange={handleFieldChange}/>
                     </InputGroup>
                 </Col>
                 <Col>
@@ -86,7 +110,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Batch Size:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="BBL or Gallons" />
+                        <Form.Control type="text" id="batchSize" placeholder="BBL or Gallons" onChange={handleFieldChange}/>
                     </InputGroup>
                 </Col>
                 <Col>
@@ -94,7 +118,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Style:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control as="select">
+                        <Form.Control as="select" id="style" onChange={handleFieldChange}>
                             <option>Pick One:</option>
                             <option>American I.P.A.</option>
                             <option>American Pale Ale</option>
@@ -110,21 +134,21 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Yeast:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="Yeast Strain" />
+                        <Form.Control type="text" id="yeast" placeholder="Yeast Strain" onChange={handleFieldChange}/>
                     </InputGroup>
                 </Col>
 
                 {grainFieldsArray.map((grainFields, idx) => {
                 return (
-                <Col key={`${grainFields}-${idx}`}>
+                <Col key={idx}>
                     <InputGroup className="mb-3" >
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Grain:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control as="select" onChange= {e => handleChange(idx, e)}>
+                        <Form.Control as="select" id={`grain-${idx}`} onChange={handleFieldChange}>
                         <option> Choose grain</option>
                         {grainSelects.map(grain => 
-                            <option key={grain.id} value={grain.name}>{grain.name}</option>
+                            <option key={grain.id} value={grain.id}>{grain.name}</option>
                         )}
                         </Form.Control>
                     </InputGroup>
@@ -132,7 +156,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Weight:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="Numbers Only" />
+                        <Form.Control type="text" placeholder="Numbers Only" id={`grain-${idx}Weight`}  key={idx} onChange= {handleFieldChange}/>
                     </InputGroup>
                     <button type="button" onClick={() => handleRemove(idx)}>X</button>
                 </Col>
@@ -186,8 +210,8 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Hops:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control type="text" placeholder="Hop Schedule" />
-                        <Form.Control type="text" placeholder="IBU's" />
+                        <Form.Control type="text" id="hop" placeholder="Hop Schedule" onChange={handleFieldChange} />
+                        <Form.Control type="text" id="ibu" placeholder="IBU's" onChange={handleFieldChange} />
                     </InputGroup>
                 </Col>
                 <Col>
@@ -195,7 +219,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Tasting Notes:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control as="textarea" placeholder="The Good, The Bad, The Ugly" />
+                        <Form.Control as="textarea" id="tastingNote" placeholder="The Good, The Bad, The Ugly" onChange={handleFieldChange}/>
                     </InputGroup>
                 </Col>
                 <Col>
@@ -203,7 +227,7 @@ const NewBrand = () => {
                         <InputGroup.Prepend>
                             <InputGroup.Text id="basic-newBrandForm">Production Status:</InputGroup.Text>
                         </InputGroup.Prepend>
-                        <Form.Control as="select">
+                        <Form.Control as="select" id="status" onChange={handleFieldChange}>
                             <option>Pick One:</option>
                             <option>Staple</option>
                             <option>Seasonal</option>
